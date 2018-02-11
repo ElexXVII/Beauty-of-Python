fab01ab39f16315911b5edf67c1b22f7b79f4fc7
ElexXVII <34800257+ElexXVII@users.noreply.github.com>
2018-02-11 22:35:58 +0100

    Update GitHub - Explosion-like Turtle Drawing.py

--- GitHub - Explosion-like Turtle Drawing.py
+++ GitHub - Explosion-like Turtle Drawing.py
@@ -6,20 +6,20 @@ degrees()
 
 for i in range(360):
 		 
-		 # Random length of each line
+     # Random length of each line
      n = r.randint(7,20)
      
-		 # Choose a color for each part of eacg line and draw each one
+     # Choose a color for each part of eacg line and draw each one
      for i in range(n):
-		 
+
           x = format(r.randint(0,255),"02x")
 					
           color("#ff"+x+"00") # Red to Yellow
-					color("#00"+x+"ff") # Blue to Cyan
+          color("#00"+x+"ff") # Blue to Cyan
 					
           forward(20)
 		 
-		 # Replace turtle on the center to draw next line
+     # Replace turtle on the center to draw next line
      penup()
      left(1)
      setx(0)
